caea2b13f031515b9a223dcd3ea4f1fc4cb7af71
ridhisud <150364040+ridhisud@users.noreply.github.com>
2023-12-25 14:20:14 +0530

    Add files via upload

---
+++ 22factorial.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int factorial(int n)
+{
+    if(n==1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n*factorial(n-1);
+    }
+}
+void main()
+{
+    int n, fact;
+    printf("Enter the number for which the factorial is to be calculated : ");
+    scanf("%d",&n);
+    fact=factorial(n);
+    printf("Factorial of %d is = %d",n,fact);
+}
