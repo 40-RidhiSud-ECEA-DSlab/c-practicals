68bb0de45a5f2d7821fa7844a0d58cac00fb2a71
ridhisud <150364040+ridhisud@users.noreply.github.com>
2023-12-25 14:38:20 +0530

    Add files via upload

---
+++ 25sumofdiagonals.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+void sumOfDiagonal(int n, int a[n][n])
+{
+    int sum=0;
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            if(i==j)
+            {
+                sum+=a[i][j];
+            }
+        }
+    }
+    printf("The sum of diagonal is = %d",sum);
+}
+void main()
+{
+    int n;
+    printf("Enter the size of the array : ");
+    scanf("%d",&n);
+    int a[n][n];
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            printf("Enter the element a[%d][%d] : ",i,j);
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            printf("%d ",a[i][j]);
+        }
+        printf("\n");
+    }
+    sumOfDiagonal(n,a);
+}
